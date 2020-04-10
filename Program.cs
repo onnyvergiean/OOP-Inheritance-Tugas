3503c8f5d8296edd95860bcb0ff5563b8658abac
onny <onny.s@students.amikom.ac.id>
2020-04-10 16:43:23 +0700

    Insialisasi Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceTugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
